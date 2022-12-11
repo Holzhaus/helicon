eefa7d722297ec3f0e33ec94d9341cb2e9a1fc1f
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-12-11 23:45:23 +0100

    feat(id3): Add mapping for ID3 tags

--- src/tag/id3.rs
+++ src/tag/id3.rs
@@ -35,13 +35,132 @@ impl ID3v2Tag {
     }
 
     /// Get the ID3 frame for a tag key.
-    fn tag_key_to_frame(key: &TagKey) -> Option<FrameId<'_>> {
+    fn tag_key_to_frame(&self, key: &TagKey) -> Option<FrameId<'_>> {
+        #[allow(clippy::match_same_arms)]
         match key {
+            TagKey::AcoustId => FrameId::ExtendedText("Acoustid Id").into(),
+            TagKey::AcoustIdFingerprint => FrameId::ExtendedText("Acoustid Fingerprint").into(),
             TagKey::Album => FrameId::Text("TALB").into(),
             TagKey::AlbumArtist => FrameId::Text("TPE2").into(),
+            TagKey::AlbumArtistSortOrder => FrameId::Text("TSO2").into(),
+            TagKey::AlbumSortOrder => FrameId::Text("TSOA").into(),
+            TagKey::Arranger => None, // TODO: Add mapping to "TIPL:arranger" (ID3v2.4) or "IPLS:arranger" (ID3v2.3)
             TagKey::Artist => FrameId::Text("TPE1").into(),
+            TagKey::ArtistSortOrder => FrameId::Text("TSOP").into(),
+            TagKey::Artists => FrameId::ExtendedText("ARTISTS").into(),
+            TagKey::Asin => FrameId::ExtendedText("ASIN").into(),
+            TagKey::Barcode => FrameId::ExtendedText("BARCODE").into(),
+            TagKey::Bpm => FrameId::Text("TBPM").into(),
+            TagKey::CatalogNumber => FrameId::ExtendedText("CATALOGNUMBER").into(),
+            TagKey::Comment => None, // Add mapping to "COMM:description"
+            TagKey::Compilation => FrameId::Text("TCMP").into(),
+            TagKey::Composer => FrameId::Text("TCOM").into(),
+            TagKey::ComposerSortOrder => FrameId::Text("TSOC").into(),
+            TagKey::Conductor => FrameId::Text("TPE3").into(),
+            TagKey::Copyright => FrameId::Text("TCOP").into(),
+            TagKey::Director => FrameId::ExtendedText("DIRECTOR").into(),
+            TagKey::DiscNumber => FrameId::Text("TPOS").into(),
+            TagKey::DiscSubtitle => match self.data.version() {
+                id3::Version::Id3v22 | id3::Version::Id3v23 => None,
+                id3::Version::Id3v24 => FrameId::Text("TSST").into(),
+            },
+            TagKey::EncodedBy => FrameId::Text("TENC").into(),
+            TagKey::EncoderSettings => FrameId::Text("TSSE").into(),
+            TagKey::Engineer => None, // TODO: Add mapping to "TIPL:engineer" (ID3v2.4) or "IPLS:engineer" (ID3v2.3)
+            TagKey::GaplessPlayback => None,
+            TagKey::Genre => FrameId::Text("TCON").into(),
+            TagKey::Grouping => FrameId::Text("TIT1").into(), // TODO: Add mapping to "GRP1", too?
+            TagKey::InitialKey => FrameId::Text("TKEY").into(),
+            TagKey::Isrc => FrameId::Text("TSRC").into(),
+            TagKey::Language => FrameId::Text("TLAN").into(),
+            TagKey::License => None, // TODO: Add mapping to "WCOP" (single URL) or "TXXX:LICENSE" (multiple or non-URL)
+            TagKey::Lyricist => FrameId::Text("TEXT").into(),
+            TagKey::Lyrics => None, // TODO: Add mapping to USLT:description
+            TagKey::Media => FrameId::Text("TMED").into(),
+            TagKey::DjMixer => None, // TODO: Add mapping to "TIPL:DJ-mix" (ID3v2.4) or "IPLS:DJ-mix" (ID3v2.3)
+            TagKey::Mixer => None, // TODO: Add mapping to "TIPL:mix" (ID3v2.4) or "IPLS:mix" (ID3v2.3)
+            TagKey::Mood => match self.data.version() {
+                id3::Version::Id3v22 | id3::Version::Id3v23 => None,
+                id3::Version::Id3v24 => FrameId::Text("TMOO").into(),
+            },
+            TagKey::Movement => FrameId::Text("MVNM").into(),
+            TagKey::MovementCount => FrameId::Text("MVIN").into(),
+            TagKey::MovementNumber => FrameId::Text("MVIN").into(),
+            TagKey::MusicBrainzArtistId => FrameId::ExtendedText("MusicBrainz Artist Id").into(),
+            TagKey::MusicBrainzDiscId => FrameId::ExtendedText("MusicBrainz Disc Id").into(),
+            TagKey::MusicBrainzOriginalArtistId => {
+                FrameId::ExtendedText("MusicBrainz Original Artist Id").into()
+            }
+            TagKey::MusicBrainzOriginalReleaseId => {
+                FrameId::ExtendedText("MusicBrainz Original Album Id").into()
+            }
+            TagKey::MusicBrainzRecordingId => None, // TODO: Add mapping to "UFID:http://musicbrainz.org"
+            TagKey::MusicBrainzReleaseArtistId => {
+                FrameId::ExtendedText("MusicBrainz Album Artist Id").into()
+            }
+            TagKey::MusicBrainzReleaseGroupId => {
+                FrameId::ExtendedText("MusicBrainz Release Group Id").into()
+            }
             TagKey::MusicBrainzReleaseId => FrameId::ExtendedText("MusicBrainz Album Id").into(),
-            _ => None,
+            TagKey::MusicBrainzTrackId => {
+                FrameId::ExtendedText("MusicBrainz Release Track Id").into()
+            }
+            TagKey::MusicBrainzTrmId => FrameId::ExtendedText("MusicBrainz TRM Id").into(),
+            TagKey::MusicBrainzWorkId => FrameId::ExtendedText("MusicBrainz Work Id").into(),
+            TagKey::MusicIpFingerprint => FrameId::ExtendedText("MusicMagic Fingerprint").into(),
+            TagKey::MusicIpPuid => FrameId::ExtendedText("MusicIP PUID").into(),
+            TagKey::OriginalAlbum => FrameId::Text("TOAL").into(),
+            TagKey::OriginalArtist => FrameId::Text("TOPE").into(),
+            TagKey::OriginalFilename => FrameId::Text("TOFN").into(),
+            TagKey::OriginalReleaseDate => match self.data.version() {
+                id3::Version::Id3v22 => None,
+                id3::Version::Id3v23 => FrameId::Text("TORY").into(),
+                id3::Version::Id3v24 => FrameId::Text("TDOR").into(),
+            },
+            TagKey::OriginalReleaseYear => None,
+            TagKey::Performer => None, // TODO: Add mapping to "TMCL:instrument" (ID3v2.4) or "IPLS:instrument" (ID3v2.3)
+            TagKey::Podcast => None,
+            TagKey::PodcastUrl => None,
+            TagKey::Producer => None, // TODO: Add mapping to "TIPL:producer" (ID3v2.4) or "IPLS:producer" (ID3v2.3)
+            TagKey::Rating => FrameId::Text("POPM").into(),
+            TagKey::RecordLabel => FrameId::Text("TPUB").into(),
+            TagKey::ReleaseCountry => {
+                FrameId::ExtendedText("MusicBrainz Album Release Country").into()
+            }
+            TagKey::ReleaseDate => match self.data.version() {
+                id3::Version::Id3v22 => None,
+                id3::Version::Id3v23 => FrameId::Text("TDAT").into(),
+                id3::Version::Id3v24 => FrameId::Text("TDRC").into(),
+            },
+            TagKey::ReleaseYear => match self.data.version() {
+                id3::Version::Id3v22 | id3::Version::Id3v24 => None,
+                id3::Version::Id3v23 => FrameId::Text("TYER").into(),
+            },
+            TagKey::ReleaseStatus => FrameId::ExtendedText("MusicBrainz Album Status").into(),
+            TagKey::ReleaseType => FrameId::ExtendedText("MusicBrainz Album Type").into(),
+            TagKey::Remixer => FrameId::Text("TPE4").into(),
+            TagKey::ReplayGainAlbumGain => FrameId::ExtendedText("REPLAYGAIN_ALBUM_GAIN").into(),
+            TagKey::ReplayGainAlbumPeak => FrameId::ExtendedText("REPLAYGAIN_ALBUM_PEAK").into(),
+            TagKey::ReplayGainAlbumRange => FrameId::ExtendedText("REPLAYGAIN_ALBUM_RANGE").into(),
+            TagKey::ReplayGainReferenceLoudness => {
+                FrameId::ExtendedText("REPLAYGAIN_REFERENCE_LOUDNESS").into()
+            }
+            TagKey::ReplayGainTrackGain => FrameId::ExtendedText("REPLAYGAIN_TRACK_GAIN").into(),
+            TagKey::ReplayGainTrackPeak => FrameId::ExtendedText("REPLAYGAIN_TRACK_PEAK").into(),
+            TagKey::ReplayGainTrackRange => FrameId::ExtendedText("REPLAYGAIN_TRACK_RANGE").into(),
+            TagKey::Script => FrameId::ExtendedText("SCRIPT").into(),
+            TagKey::ShowName => None,
+            TagKey::ShowNameSortOrder => None,
+            TagKey::ShowMovement => FrameId::ExtendedText("SHOWMOVEMENT").into(),
+            TagKey::Subtitle => FrameId::Text("TIT3").into(),
+            TagKey::TotalDiscs => FrameId::Text("TPOS").into(),
+            TagKey::TotalTracks => FrameId::Text("TRCK").into(),
+            TagKey::TrackNumber => FrameId::Text("TRCK").into(),
+            TagKey::TrackTitle => FrameId::Text("TIT2").into(),
+            TagKey::TrackTitleSortOrder => FrameId::Text("TSOT").into(),
+            TagKey::ArtistWebsite => FrameId::Text("WOAR").into(),
+            TagKey::WorkTitle => FrameId::ExtendedText("WORK TIT1").into(),
+            TagKey::Writer => FrameId::ExtendedText("Writer").into(),
         }
     }
 
@@ -71,9 +190,10 @@ impl Tag for ID3v2Tag {
     }
 
     fn get(&self, key: &TagKey) -> Option<&str> {
-        Self::tag_key_to_frame(key).and_then(|frame_id| match frame_id {
-            FrameId::Text(value) => self.get(value),
-            FrameId::ExtendedText(value) => self.get_extended_text(value),
-        })
+        self.tag_key_to_frame(key)
+            .and_then(|frame_id| match frame_id {
+                FrameId::Text(value) => self.get(value),
+                FrameId::ExtendedText(value) => self.get_extended_text(value),
+            })
     }
 }
