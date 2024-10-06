651b7251a180827175c5f967bfdd80e305869acf
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2024-10-06 22:50:22 +0200

    feat(handle_candidate): Show "(id)" indicator on track if details hidden

--- src/cli/ui/handle_candidate.rs
+++ src/cli/ui/handle_candidate.rs
@@ -215,6 +215,8 @@ pub fn show_candidate<B: ReleaseLike, C: ReleaseLike>(
                 (!track_similarity.is_track_number_equal()).then_some("#"),
                 (!track_similarity.is_track_title_equal()).then_some("title"),
                 (!track_similarity.is_track_length_equal()).then_some("length"),
+                (!show_details && !track_similarity.is_musicbrainz_recording_id_equal())
+                    .then_some("id"),
             ]
             .into_iter()
             .flatten()
